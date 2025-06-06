114ccec93ee455a688e9cb4b83c2546c91d29e53
andr36s <andres40106@gmail.com>
2025-06-06 15:11:44 -0500

    Elimino archivos basura del proyecto, ademas agrego lista que permita agregar y borrar datos

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" placeholder="Nueva tarea" @keyup.enter="agregarTarea">
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{ tarea }}
+                <button @click="borrarTarea(index)">Borrar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    const tareas = ref([]);
+    const nuevaTarea = ref('');
+
+    const agregarTarea = () => {
+        tareas.value.push(nuevaTarea.value);
+        nuevaTarea.value = '';
+    }
+const borrarTarea = (index) => {
+        tareas.value.splice(index, 1);
+    }
+    
+</script>
+
+<style>
+
+</style>
