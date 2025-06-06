39202649533ab4de7a623cfaf773746698c19e08
andr36s <andres40106@gmail.com>
2025-06-06 16:03:55 -0500

    Uso de router link y Views

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="sidebar">
+    <h2>Barra lateral</h2>
+    <RouterLink to="/contador">Contador</RouterLink>
+    <RouterLink to="/lista-de-tareas">Lista de Tareas</RouterLink>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar {
+    width: 200px;
+    background-color: #f0f0f0;
+    padding: 20px;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+    <div class="content">
+
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.container {
+    display: flex;
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+</style>
