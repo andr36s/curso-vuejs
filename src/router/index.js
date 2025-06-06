c805cf928aa410dbaebd54a82c11a57474c8d866
andr36s <andres40106@gmail.com>
2025-06-06 17:17:26 -0500

    Creacion y asignacion de la carpeta modules

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,12 @@ const router = createRouter({
     {
       path: '/contador',
       name: 'contador',
-      component: () => import('../components/Contador.vue'),
+      component: () => import('../modules/contador/components/Contador.vue'),
     },
     {
       path: '/lista-de-tareas',
       name: 'lista-de-tareas',
-      component: () => import('../components/ListaDeTareas.vue'),
+      component: () => import('../modules/listaDeTareas/components/ListaDeTareas.vue'),
     },
   ],
 })
