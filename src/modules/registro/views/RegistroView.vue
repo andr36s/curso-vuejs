64eb9bb4fda384da2ed10b8b37ef2d43a020e486
andr36s <andres40106@gmail.com>
2025-06-06 19:28:34 -0500

    Agregamos vee-validate al form

--- src/modules/registro/views/RegistroView.vue
+++ src/modules/registro/views/RegistroView.vue
@@ -1,16 +1,29 @@
 <template>
     <div>
         <h2>Formulario de Registro</h2>
-        <input type="text" placeholder="Ingrese nombre" />
-        <input type="email" placeholder="Ingrese correo electrónico" />
-        <button type="submit">Registrar</button>
+        <Form>
+
+            <div class="form">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="Ingrese nombre" />
+            </div>
+            <div class="form">
+                <label for="correo">Correo:</label>
+                <input type="email" placeholder="Ingrese correo electrónico" />
+            </div>
+            <div class="form">
+                <button type="submit">Registrar</button>
+            </div>
+        </Form>
     </div>
 </template>
 
 <script setup>
-
+    import {Form, Field} from 'vee-validate';
 </script>
 
 <style scoped>
-
+.form {
+    margin-bottom: 10px;
+}
 </style>
