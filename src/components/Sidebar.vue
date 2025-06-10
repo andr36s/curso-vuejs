b18533546f239a56f0455bd8031b94224c5f5ef4
andr36s <andres40106@gmail.com>
2025-06-10 16:41:36 -0500

    En esta parte no era necesario usar watch ya que las variables por si solas son reactivas, por lo tanto ajustamos el codigo

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="sidebar">
     <h2>Bienvenido</h2>
-    <h3>Nombre: {{ nombre }}</h3>
-    <h3>Email: {{ correo }}</h3>
+    <h3>Nombre: {{ registrarStore.nombre }}</h3>
+    <h3>Email: {{ registrarStore.correo }}</h3>
     <ul>
         <li><RouterLink to="/registrar">Registrar</RouterLink></li>
         <li><RouterLink to="/contador">Contador</RouterLink></li>
@@ -13,18 +13,8 @@
 
 <script setup>
 import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
-import { ref, watch } from 'vue';
 
 const registrarStore = useRegistrarStore();
-const nombre = ref(registrarStore.nombre)
-const correo = ref(registrarStore.correo)
-
-watch(() => registrarStore.nombre, (newNombre) => {
-    nombre.value = newNombre;
-});
-watch(() => registrarStore.correo, (newCorreo) => {
-    correo.value = newCorreo;
-})
 
 </script>
 
